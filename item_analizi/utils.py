71125dd5068b5f002d4a753092f4a258b5b8f2f4
Mustafa <learnbtt@gmail.com>
2025-02-10 05:10:44 +0300

    Delete item_analizi directory

--- item_analizi/utils.py
+++
@@ -1,9 +0,0 @@
-# utils.py
-
-def format_price_with_dots_and_yang(value: float) -> str:
-    # Binlik ayıracı nokta, sonda ' Yang' gibi...
-    pass
-
-def parse_price_from_string(formatted_str: str) -> float:
-    # "1.200.000 Yang" -> 1200000.0
-    pass
